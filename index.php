35acade5387c780759465ebdad49d6d1772e731f
Matthew Arnold <matt_a70@hotmail.com>
2022-02-05 19:56:22 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              N/A
+ * @since             1.0.0
+ * @package           Peak_Dashboard
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Peak Bagging Dashboard
+ * Plugin URI:        N/A
+ * Description:       Historic County Tops of the United Kingdom
+ * Version:           1.0.0
+ * Author:            Matt
+ * Author URI:        N/A
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       peak-dashboard
+ * Domain Path:       /languages
+ */
+
+
+
+
+ ?>
